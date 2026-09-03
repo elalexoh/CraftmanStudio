b4438319526ff00b03e6edeb751bbb773eab41f4
Frederick A. Gonzalez <g7.alexander@gmail.com>
2026-09-02 20:36:57 -0400

    fix(drawing-tools): restore opening script tag in DrawingTools.vue

--- src/components/DrawingTools.vue
+++ src/components/DrawingTools.vue
@@ -1,3 +1,4 @@
+<script setup lang="ts">
 import { usePainting } from '../composables/usePainting';
 import { useRulers } from '../composables/useRulers';
 import { useCameraPresets } from '../composables/useCameraPresets';
